0a8ea4f38c46f17190f7bd6a3adeaf586149fe66
Sergey Vartanov <me@enzet.ru>
2020-02-06 00:09:52 +0300

    Refactor tests.

--- test.py
+++ test.py
@@ -4,22 +4,6 @@ from emmio.text import Text
 from emmio.frequency import FrequencyList
 
 
-class DictionaryTest:
-    def __init__(self, file_name: str, format_: str) -> None:
-        """
-        :param file_name: dictionary file path.
-        :param format_: dictionary format: `dict`, `json`, or `yaml`.
-        """
-        self.file_name = file_name
-        self.format_ = format_
-
-    def test_run(self) -> None:
-        dictionary = Dictionary(self.file_name, self.format_)
-        assert dictionary.get("other") is None
-        assert dictionary.get("книга") == "    book\n"
-        assert dictionary.get("письмо") == "    letter\n"
-
-
 def check(lexicon: Lexicon) -> None:
     assert lexicon.has("книга")
     assert lexicon.has("письмо")
@@ -30,27 +14,27 @@ def check(lexicon: Lexicon) -> None:
     assert lexicon.get("Иван") == LexiconResponse.NOT_A_WORD
 
 
-class LexiconTest:
-    def __init__(self, language: str, lexicon_file_name: str) -> None:
-        self.language = language
-        self.lexicon_file_name = lexicon_file_name
-        lexicon = Lexicon(self.language, self.lexicon_file_name)
+def do_lexicon(language: str, lexicon_file_name: str) -> None:
+    lexicon = Lexicon(language, lexicon_file_name)
 
-        lexicon.read()
-        check(lexicon)
+    lexicon.read()
+    check(lexicon)
 
-        lexicon.file_name = "test/temp_lexicon.json"
-        lexicon.write()
-        lexicon.read()
-        check(lexicon)
+    lexicon.file_name = "test/temp_lexicon.json"
+    lexicon.write()
+    lexicon.read()
+    check(lexicon)
 
 
 def test_dict() -> None:
-    DictionaryTest("test/simple.dict", "dict").test_run()
+    dictionary = Dictionary("test/simple.dict", "dict")
+    assert dictionary.get("other") is None
+    assert dictionary.get("книга") == "    book\n"
+    assert dictionary.get("письмо") == "    letter\n"
 
 
 def test_lexicon() -> None:
-    LexiconTest("ru", "test/lexicon.json")
+    do_lexicon("ru", "test/lexicon.json")
 
 
 def test_text() -> None:
@@ -61,5 +45,6 @@ def test_text() -> None:
     assert frequency_list.get_occurrences("it") == 1
     assert frequency_list.get_occurrences("it’s") == 0
     assert frequency_list.get_occurrences("It") == 0
-
     assert frequency_list.get_occurrences("the") == 2
+    assert frequency_list.get_occurrences("s") == 2
+    assert len(frequency_list) == 9
