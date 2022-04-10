3320c14c7d23ea15bdbfa478dde3086bc5493938
Sergey Vartanov <me@enzet.ru>
2022-04-11 00:52:02 +0300

    Add separate download function.

--- emmio/util.py
+++ emmio/util.py
@@ -3,10 +3,15 @@ Emmio.
 
 Utility functions.
 """
+import sys
 from datetime import datetime, timedelta
 
 __author__ = "Sergey Vartanov"
 __email__ = "me@enzet.ru"
+
+from pathlib import Path
+
+from urllib3 import PoolManager, HTTPResponse
 
 
 def day_start(point: datetime) -> datetime:
@@ -41,3 +46,34 @@ def plus_month(point: datetime) -> datetime:
 def first_day_of_week(point: datetime) -> datetime:
     day = point.date() - timedelta(days=point.weekday())
     return datetime.combine(day, datetime.min.time())
+
+
+def download(
+    address: str,
+    cache_path: Path,
+    buffer_size: int = 400_000
+) -> bytes:
+
+    sys.stdout.write(f"Downloading {address}: ")
+    sys.stdout.flush()
+
+    pool_manager: PoolManager = PoolManager()
+    result: HTTPResponse = pool_manager.request(
+        "GET", address, preload_content=False
+    )
+    pool_manager.clear()
+    data: bytearray = bytearray()
+    while True:
+        buffer: bytes = result.read(buffer_size)
+        if not buffer:
+            break
+        sys.stdout.write("█")
+        sys.stdout.flush()
+        data.extend(buffer)
+    sys.stdout.write("\n")
+
+    with cache_path.open("wb+") as temp_file:
+        temp_file.write(data)
+
+    return data
+
