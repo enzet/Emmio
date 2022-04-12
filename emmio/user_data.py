ab8cceb0e2316f46d89dfc0b1d17902eff9250bb
Sergey Vartanov <me@enzet.ru>
2022-04-13 02:38:16 +0300

    Fix frequency list extraction.

--- emmio/user_data.py
+++ emmio/user_data.py
@@ -1,14 +1,16 @@
 import json
-from os import listdir
-from os.path import join
+from pathlib import Path
 from typing import Any, Iterator, Optional
 
 from emmio.language import Language, construct_language
 from emmio.learning import Learning
+from emmio.frequency import FrequencyList, MalformedFile
 from emmio.lexicon import Lexicon
 
 __author__ = "Sergey Vartanov"
 __email__ = "me@enzet.ru"
+
+from emmio.ui import error
 
 
 class UserData:
@@ -18,15 +20,18 @@ class UserData:
 
     def __init__(self):
         self.courses = {}
-        self.lexicons = {}
-        self.path: Optional[str] = None
+        self.lexicons: dict[Language, Lexicon] = {}
+        self.frequency_lists: dict[str, FrequencyList] = {}
+        self.path: Optional[Path] = None
 
         self.course_ids: set[str] = set()
 
         self.lexicon_config: dict[str, str] = {}
+        self.frequency_list_config: dict[str, dict[str, str]] = {}
+        self.learn_config: dict[str, dict[str, str]] = {}
 
     @classmethod
-    def from_directory(cls, path: str):
+    def from_directory(cls, path: Path, user_id: str):
         """
         :param path: path to the user data directory
         """
@@ -34,27 +39,43 @@ class UserData:
 
         user_data.path = path
 
-        with open(join(path, "config.json")) as config_file:
+        with (path / user_id / "config.json").open() as config_file:
             config: dict[str, Any] = json.load(config_file)
-            user_data.id_ = config["id"]
+            user_data.id_ = user_id
             user_data.name = config["name"]
             user_data.native_languages = set(
                 construct_language(x) for x in config["native_languages"]
             )
             user_data.lexicon_config = config["lexicon"]
-
-        for directory_name in listdir(path):  # type: str
-            if directory_name == "learn":
-                for file_name in listdir(
-                    join(path, directory_name)
-                ):  # type: str
-                    if file_name.endswith(".json"):
-                        user_data.course_ids.add(file_name[: -len(".json")])
+            user_data.learn_config = config["learn"]
+            user_data.frequency_list_config = config["priority"]
+            user_data.course_ids = set(config["learn"].keys())
 
         return user_data
 
-    def get_frequency_list_id(self, language: Language) -> str:
-        return self.lexicon_config[language.get_code()]
+    def get_frequency_list_for_lexicon(self, language: Language) -> FrequencyList:
+        return self.get_frequency_list(
+            self.lexicon_config[language.get_code()]
+        )
+
+    def get_frequency_list_structure(
+        self, frequency_list_id: str
+    ) -> dict[str, str]:
+        return self.frequency_list_config[frequency_list_id]
+
+    def get_frequency_list(self, frequency_list_id: str) -> FrequencyList:
+        if frequency_list_id not in self.frequency_lists:
+            try:
+                frequency_list: FrequencyList = FrequencyList.from_structure(
+                    self.frequency_list_config[frequency_list_id],
+                    self.path / "priority",
+                )
+                self.frequency_lists[frequency_list_id] = frequency_list
+            except MalformedFile:
+                error("cannot construct frequency list")
+                return None
+
+        return self.frequency_lists[frequency_list_id]
 
     def get_lexicon_languages(self) -> Iterator[Language]:
         return map(construct_language, self.lexicon_config.keys())
