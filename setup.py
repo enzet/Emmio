721f64ac8dafbad11dae76da04891e3d4ee0674b
Sergey Vartanov <me@enzet.ru>
2022-04-17 21:46:49 +0300

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="Emmio",
+    version="0.1",
+    packages=["emmio"],
+    url="https://github.com/enzet/Emmio",
+    license="",
+    author="Sergey Vartanov",
+    author_email="me@enzet.ru",
+    description="Language learning toolbox",
+    entry_points={
+        "console_scripts": ["emmio=emmio.main:main"],
+    },
+)
