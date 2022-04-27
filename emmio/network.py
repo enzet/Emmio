d2418eae70a0664e018a164fcf1f83077e92ab9e
Sergey Vartanov <me@enzet.ru>
2022-04-28 02:32:53 +0300

    Remove redundant functions

--- emmio/network.py
+++ emmio/network.py
@@ -4,10 +4,9 @@ Emmio.
 Utility for network connections using urllib3.
 """
 import json
-import os
 import time
 from datetime import datetime, timedelta
-from typing import Any, Optional
+from typing import Any
 
 import urllib3
 
@@ -91,74 +90,3 @@ def write_cache(data: bytes, kind: str, cache_file_name: str) -> Any:
         error(f"unknown data format {kind}")
 
     return None
-
-
-def get_content(
-    address: str,
-    parameters: dict[str, str],
-    cache_file_name: Optional[str],
-    kind: str,
-    is_secure: bool,
-    name: str = None,
-    update_cache: bool = False,
-) -> Any:
-    """
-    Read content from URL or from cached file.
-
-    :param address: first part of URL without "http://"
-    :param parameters: URL parameters
-    :param cache_file_name: name of cache file
-    :param kind: type of content: `html` or `json`
-    :param is_secure: use `https://` instead of `http://`.
-    :param name: short request name to display in logs.
-    :param update_cache: rewrite cache file.
-    :return: content if exist.
-    """
-    if cache_file_name is not None:
-        os.makedirs(os.path.dirname(cache_file_name), exist_ok=True)
-
-    # Read from the cache file.
-
-    if (
-        cache_file_name is not None
-        and not update_cache
-        and os.path.isfile(cache_file_name)
-    ):
-        with open(cache_file_name) as cache_file:
-            if kind == "json":
-                try:
-                    return json.load(cache_file)
-                except ValueError:
-                    return None
-            elif kind == "html":
-                return cache_file.read()
-            else:
-                error(f"unknown data format {kind}")
-
-    # Read from the network.
-
-    try:
-        data: bytes = get_data(
-            address, parameters, is_secure=is_secure, name=name
-        )
-        if cache_file_name is not None:
-            return write_cache(data, kind, cache_file_name)
-    except Exception as e:
-        print(e)
-        return None
-
-    return None
-
-
-def get_file(path: str) -> Optional[str]:
-    """Get file content from the network."""
-    if path.startswith("https://"):
-        path = path[len("https://") :]
-        is_secure = True
-    elif path.startswith("http://"):
-        path = path[len("http://") :]
-        is_secure = False
-    else:
-        return None
-
-    return get_content(path, {}, path, "html", is_secure, path)
