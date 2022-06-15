79313b9658aec61e4d674e5639e91d2813dd785b
Sergey Vartanov <me@enzet.ru>
2022-06-15 23:49:25 +0300

    Add entry point documentation

--- emmio/__main__.py
+++ emmio/__main__.py
@@ -1,3 +1,4 @@
+"""Emmio entry point."""
 import sys
 import threading
 from pathlib import Path
@@ -9,8 +10,12 @@ from emmio.server import TelegramServer
 from emmio.ui import set_log, Logger
 from emmio.user_data import UserData
 
+__author__ = "Sergey Vartanov"
+__email__ = "me@enzet.ru"
+
 
 def main():
+    """Emmio entry point."""
     set_log(Logger)
 
     data_path: Path = Path(sys.argv[1])
@@ -24,6 +29,7 @@ def main():
 
     @bot.message_handler(commands=["start"])
     def start(message: Message):
+        """Start Emmio process."""
         bot.send_message(message.chat.id, "Welcome to Emmio.")
         bot.register_next_step_handler(message, server.receive_message)
         server.id_ = message.chat.id
