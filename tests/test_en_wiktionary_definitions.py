27f2cdb17e2b5243908129803f16d3307458a1ea
Sergey Vartanov <me@enzet.ru>
2022-06-16 03:39:30 +0300

    Rename definition tests

--- tests/test_en_wiktionary_definitions.py
+++ tests/test_en_wiktionary_definitions.py
@@ -10,7 +10,7 @@ dictionary: EnglishWiktionary = EnglishWiktionary(
 )
 
 
-def check_parse_definition(text: str, value: tuple) -> None:
+def check_form(text: str, value: tuple) -> None:
     definition: Definition = Definition(
         [DefinitionValue(x[0], "" if len(x) == 1 else x[1]) for x in value[1]],
         value[0],
@@ -18,36 +18,36 @@ def check_parse_definition(text: str, value: tuple) -> None:
     assert dictionary.process_definition_2(text) == definition
 
 
-def check_parse_link(text: str, link_type: str, link_value: str) -> None:
+def check_link(text: str, link_type: str, link_value: str) -> None:
     assert dictionary.process_definition_2(text) == Link(link_type, link_value)
 
 
-def test_parse_definition_1() -> None:
-    check_parse_definition("above", ([], [["above"]]))
+def test_definition() -> None:
+    check_form("above", ([], [["above"]]))
 
 
-def test_parse_definition_2() -> None:
-    check_parse_definition("to stay, remain", ([], [["to stay"], ["remain"]]))
+def test_definition_separated_by_comma() -> None:
+    check_form("to stay, remain", ([], [["to stay"], ["remain"]]))
 
 
-def test_parse_definition_3() -> None:
-    check_parse_definition("(regional) to live", (["regional"], [["to live"]]))
+def test_definition_separated_by_colon() -> None:
+    check_form("lecture; talk", ([], [["lecture"], ["talk"]]))
 
 
-def test_parse_definition_7() -> None:
-    check_parse_definition("(Louisiana) diaper", (["Louisiana"], [["diaper"]]))
+def test_definition_with_descriptor() -> None:
+    check_form("(regional) to live", (["regional"], [["to live"]]))
 
 
-def test_parse_definition_4() -> None:
-    check_parse_definition(
+def test_definition_with_place_descriptor() -> None:
+    check_form("(Louisiana) diaper", (["Louisiana"], [["diaper"]]))
+
+
+def test_separated_definitions_with_descriptor() -> None:
+    check_form(
         "(colloquial) you know, like, y'know.",
         (["colloquial"], [["you know"], ["like"], ["y'know"]]),
     )
 
 
-def test_parse_definition_5() -> None:
-    check_parse_link("plural of média", "plural", "média")
-
-
-def test_parse_definition_6() -> None:
-    check_parse_definition("lecture; talk", ([], [["lecture"], ["talk"]]))
+def test_link() -> None:
+    check_link("plural of média", "plural", "média")
