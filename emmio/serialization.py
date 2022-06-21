2415cc4da8bc53599f4d88af81633b07bc107832
Sergey Vartanov <me@enzet.ru>
2022-06-21 03:08:24 +0300

    Add decoder for serialization

--- emmio/serialization.py
+++ emmio/serialization.py
@@ -6,7 +6,7 @@ ENDIAN: str = "little"
 
 
 class Encoder:
-    """Encoding lexicon structure."""
+    """Encoding structure."""
 
     MAGIC: bytes = b"XXXXXX"
     VERSION_MAJOR: int = 0
@@ -60,3 +60,41 @@ class Encoder:
         self.code.write(
             (values.index(value) + 1).to_bytes(1, ENDIAN, signed=False)
         )
+
+
+class Decoder:
+    """Decoding structure."""
+
+    MAGIC: bytes = b"XXXXXX"
+    VERSION_MAJOR: int = 0
+    VERSION_MINOR: int = 0
+
+    def __init__(self, code: io.BytesIO) -> None:
+        self.code: io.BytesIO = code
+
+    def decode_magic(self) -> None:
+        assert self.code.read(6) == self.MAGIC
+        assert self.decode_int(1) == self.VERSION_MAJOR
+        assert self.decode_int(1) == self.VERSION_MINOR
+
+    def decode_int(self, size: int) -> int:
+        """Decode integer value."""
+        if (value := self.code.read(size)) == b"":
+            raise EOFError()
+        return int.from_bytes(value, ENDIAN)
+
+    def decode_string(self) -> str:
+        """Decode string value."""
+        return self.code.read(self.decode_int(2)).decode()
+
+    def decode_enum(self, values) -> Optional[str]:
+        """Decode enum value."""
+        if (code := self.decode_int(1)) == 0:
+            return None
+        return values[code - 1]
+
+    def decode_ternary(self) -> Optional[bool]:
+        """Decode optional boolean value."""
+        if (code := self.code.read(1)) == b"\x02":
+            return None
+        return code == b"\x01"
