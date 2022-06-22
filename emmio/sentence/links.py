a6d63d97dcb5dd29e64a11ada669b087f8f3b1e3
Sergey Vartanov <me@enzet.ru>
2022-06-22 23:28:21 +0300

    Add links binary decoder

--- emmio/sentence/links.py
+++ emmio/sentence/links.py
@@ -1,4 +1,4 @@
-from emmio.serialization import Encoder
+from emmio.serialization import Encoder, Decoder
 
 
 class LinksEncoder(Encoder):
@@ -13,3 +13,23 @@ class LinksEncoder(Encoder):
         for key, value in links.items():
             self.encode_int(key, 4)
             self.encode_int(value, 4)
+
+
+class LinksDecoder(Decoder):
+    """Decoder for links between Tatoeba sentences."""
+
+    MAGIC: bytes = b"EMMLNK"
+    VERSION_MAJOR: int = 0
+    VERSION_MINOR: int = 1
+
+    def decode(self) -> dict[int, int]:
+        """Decode links between Tatoeba sentences from the binary format."""
+        links: dict[int, int] = {}
+
+        while True:
+            try:
+                links[self.decode_int(4)] = self.decode_int(4)
+            except EOFError:
+                break
+
+        return links
