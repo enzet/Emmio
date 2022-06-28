108f242f0244562acb4fc0ccafad472262e852e4
Sergey Vartanov <me@enzet.ru>
2022-06-28 23:58:47 +0300

    Add worker documentation

--- emmio/server.py
+++ emmio/server.py
@@ -32,20 +32,26 @@ MAXIMUM_MESSAGE_SIZE: int = 512
 
 
 class Worker:
+    """Any learning or checking worker."""
+
     def __lt__(self, other: "Worker") -> bool:
         raise NotImplementedError()
 
     def is_ready(self) -> bool:
+        """Check if worker has next message to send."""
         pass
 
     @staticmethod
     def get_greetings() -> str:
+        """Return greetings to tell that worker is ready."""
         return "Hello."
 
     def get_next_question(self) -> list[str]:
+        """Return list of next messages."""
         pass
 
     def process_answer(self, message):
+        """Process user response."""
         pass
 
 
