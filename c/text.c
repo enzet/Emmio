af7f7374b6034350f61b5feaa6a034e81db26ec8
Sergey Vartanov <me@enzet.ru>
2022-10-17 22:38:59 +0400

    Rewrite text processing in C

--- c/text.c
+++ c/text.c
@@ -1,108 +1,158 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <wchar.h>
+// The script constructs frequency list for text.
+// Arguments:
+//     - Path to input file with text.
+//     - Path to output file to store frequency list.
+
 #include <locale.h>
+#include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
 
 #define BUFFER_SIZE 1024
+
+// How many bits is sufficient to encode a character of the language.
+#define BITS_PER_CHARACTER 6
+
+// If word has more characters, than this number, it will be truncated and its
+// prefix will be used as a whole word.
 #define MAX_WORD_SIZE 50
 
+int w = 0;
+struct Occurrences* occurrences;
 
-struct Dictionary {
-    struct Dictionary* zero;
-    struct Dictionary* one;
+struct BinaryTree {
+    struct BinaryTree* subtrees[2];
     int count;
 };
 
-void print(struct Dictionary* dictionary, wchar_t* word, int index, int summ) {
+// Word and the number of its occurrences in the text.
+struct Occurrences {
+    wchar_t word[MAX_WORD_SIZE];
+    int count;
+};
 
-    if (index > 0 && index % 6 == 0) {
-        word[index / 6] = summ + L'\x0561';
-        word[index / 6 + 1] = L'\0';
+int comparator(const void *x, const void *y) {
+    return ((struct Occurrences*) y)->count - ((struct Occurrences*) x)->count;
+}
+
+void process(struct BinaryTree* binary_tree, wchar_t* word, int index,
+        int summ) {
+
+    if (index > 0 && index % BITS_PER_CHARACTER == 0) {
+        word[index / BITS_PER_CHARACTER - 1] = summ + L'\x0561';
+        word[index / BITS_PER_CHARACTER] = L'\0';
         summ = 0;
     }
-    if (dictionary == NULL) {
+    if (binary_tree == NULL) {
         return;
     }
-    if (dictionary->count > 0) {
-        printf("%ls %d\n", word, dictionary->count);
+    if (binary_tree->count > 0) {
+        occurrences[w].count = binary_tree->count;
+        wcscpy(occurrences[w].word, word);
+        w++;
     }
-    if (dictionary->one != NULL) {
-        print(dictionary->one, word, index + 1, (summ << 1) + 1);
-    }
-    if (dictionary->zero != NULL) {
-        print(dictionary->zero, word, index + 1, summ << 1);
+    for (int bit = 0; bit < 2; bit++) {
+        if (binary_tree->subtrees[bit] != NULL) {
+            process(binary_tree->subtrees[bit], word, index + 1, (summ << 1) + bit);
+        }
     }
 }
 
 int main(int argc, char** argv) {
 
-	char* input_file_path = argv[1];
-	char* output_file_path = argv[2];
-
-    FILE* file = fopen(input_file_path, "r");
-    wchar_t word[MAX_WORD_SIZE];
-    int i = 0;
-
     setlocale(LC_ALL, "");
 
-    struct Dictionary *dictionary = malloc(sizeof(struct Dictionary));
-    *dictionary = (struct Dictionary){NULL, NULL, 0};
+    char* input_file_path = argv[1];
+    char* output_file_path = argv[2];
 
-    struct Dictionary *current = dictionary;
+    FILE* input_file = fopen(input_file_path, "r");
+
+    struct BinaryTree *binary_tree = malloc(sizeof(struct BinaryTree));
+    *binary_tree = (struct BinaryTree) {{NULL, NULL}, 0};
+
+    struct BinaryTree *current = binary_tree;
 
     wchar_t buffer[BUFFER_SIZE];
-    while (fgetws(buffer, BUFFER_SIZE, file) != NULL) {
-        int j = 0;
-        while (1) {
-            wchar_t c = buffer[j];
-            if (!c) {
-                break;
+    wchar_t word[MAX_WORD_SIZE];
+    int word_index = 0;
+    int unique_words = 0;
+    int buffer_count = 0;
+
+    printf("Reading...\n");
+
+    while (fgetws(buffer, BUFFER_SIZE, input_file) != NULL) {
+
+        int buffer_index = 0;
+        wchar_t character;
+
+        buffer_count++;
+        if (buffer_count % 1000000 == 0) {
+            printf("Buffer #%d.\n", buffer_count);
+        }
+
+        while ((character = buffer[buffer_index])) {
+
+            int number = -1; // Integer representation of the character.
+
+            if (L'\x0561' <= character && character <= L'\x0587') {
+                number = character - L'\x0561';
+            } else if (L'\x0531' <= character && character <= L'\x0556') {
+                number = character - L'\x0531';
             }
-            if (L'\x0561' <= c && c <= L'\x0587') {
-                if (i < MAX_WORD_SIZE - 1) {
-                    word[i] = c;
-                    int n = c - L'\x0561';
-                    for (int k = 0x20; k >= 1; k = k >> 1) {
-                        int bit = n & k ? 1 : 0;
-                        if (bit == 0) {
-                            if (current->zero == NULL) {
-                                current->zero = malloc(sizeof(struct Dictionary));
-                                *current->zero = (struct Dictionary){NULL, NULL, 0};
-                            }
-                            current = current->zero;
-                        } else {
-                            if (current->one == NULL) {
-                                current->one = malloc(sizeof(struct Dictionary));
-                                *current->one = (struct Dictionary){NULL, NULL, 0};
-                            }
-                            current = current->one;
-                        }
-                    }
-                    i++;
+
+            // If character is of interest.
+            if (number >= 0) {
+                if (word_index > MAX_WORD_SIZE) {
+                    buffer_index++;
+                    continue;
                 }
-            } else if (L'\x0531' <= c && c <= L'\x0556') {
-                if (i < MAX_WORD_SIZE - 1) {
-                    word[i] = c + 0x30;
-                    i++;
+                word[word_index] = character;
+                word_index++;
+
+                for (int k = 1 << (BITS_PER_CHARACTER - 1); k >= 1;
+                        k = k >> 1) {
+
+                    int bit = number & k ? 1 : 0;
+                    if (!current->subtrees[bit]) {
+                        current->subtrees[bit] =
+                            malloc(sizeof(struct BinaryTree));
+                        *current->subtrees[bit] =
+                            (struct BinaryTree) {{NULL, NULL}, 0};
+                    }
+                    current = current->subtrees[bit];
                 }
             } else {
-                if (i > 0) {
+                if (word_index > 0) {
+                    if (!current->count) {
+                        unique_words++;
+                    }
                     current->count += 1;
-                    word[i] = 0;
+                    word[word_index] = 0;
                 }
-                word[0] = 0;
-                current = dictionary;
-                i = 0;
+                word[0] = L'\0';
+                current = binary_tree;
+                word_index = 0;
             }
-            j++;
+            buffer_index++;
         }
     }
-    fclose(file);
+    fclose(input_file);
 
-    wchar_t str[1000];
-    print(dictionary, str, 0, 0);
+    wchar_t str[MAX_WORD_SIZE];
+    occurrences = malloc(unique_words * sizeof(struct Occurrences));
+
+    process(binary_tree, str, 0, 0);
+
+    printf("Sorting...\n");
+    qsort((void*) occurrences, unique_words, sizeof(struct Occurrences), comparator);
+
+    FILE* output_file = fopen(output_file_path, "w");
+
+    printf("Writing...\n");
+    for (int i = 0; i < unique_words; i++) {
+        fwprintf(output_file, L"%ls %d\n", occurrences[i].word, occurrences[i].count);
+    }
+    fclose(output_file);
 
     return 0;
 }
