7d9fc61026042b76c75162a583a2535cc10707f7
Sergey Vartanov <me@enzet.ru>
2022-12-18 12:20:14 +0400

    Change logging style

--- emmio/__main__.py
+++ emmio/__main__.py
@@ -34,7 +34,11 @@ def process_list_command(data, arguments):
 def main():
     """Emmio entry point."""
 
-    coloredlogs.install(level=logging.INFO, fmt="%(message)s")
+    coloredlogs.install(
+        level=logging.INFO,
+        fmt="%(message)s",
+        level_styles=dict(info=dict(color="yellow")),
+    )
 
     parser: ArgumentParser = ArgumentParser("Emmio")
     parser.add_argument("--data", help="path to data directory")
