6bc8ffdb215834a6d9f4ca8c1639cbbd38f8d095
Sergey Vartanov <me@enzet.ru>
2024-01-02 13:00:22 +0400

    Rewrite learning visualization

--- emmio/learn/visualizer.py
+++ emmio/learn/visualizer.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import numpy as np
 from matplotlib import pyplot as plt, dates as mdates, transforms as mtransforms
 
-from emmio.learn.core import LearningRecord, Knowledge
+from emmio.learn.core import LearningRecord, Knowledge, Learning
 
 DEPTH_COLORS_BLACK_AND_WHITE: list[str] = ["#000000"] + [
     "#" + f"{hex(int(256 - 256 / 1.5 ** x))[2:]:2}".replace(" ", "0") * 3
@@ -31,7 +31,7 @@ def get_depth(interval: timedelta) -> int:
 @dataclass
 class LearningVisualizer:
     # User responses.
-    records: list[LearningRecord]
+    records: list[tuple[LearningRecord, Learning]]
 
     # If true, use time as x-axis, otherwise use actions count.
     is_time: bool = False
@@ -39,6 +39,7 @@ class LearningVisualizer:
     # Depth labels.
     show_text: bool = False
 
+    by_language: bool = False
     count_by_depth: bool = False
     show_not_learning: bool = False
     color_mode: str = "depth_colors"
@@ -61,6 +62,8 @@ class LearningVisualizer:
         knowledges = {}
 
         def compute_data_id() -> str | int:
+            if self.by_language:
+                return learning.learning_language.get_code()
             if self.use_subtypes:
                 return (
                     f"{get_depth(knowledges[record.question_id].interval):05},"
@@ -75,14 +78,17 @@ class LearningVisualizer:
 
         # Compute data for the plot.
 
-        for record in self.records:
+        for record, learning in self.records:
             if record.question_id in knowledges:
                 data[compute_data_id()] -= (
                     1 / (2 ** knowledges[record.question_id].get_depth())
                     if self.count_by_depth
                     else 1
                 )
-            if not record.is_learning():
+            if (
+                record.question_id in knowledges
+                and not knowledges[record.question_id].is_learning()
+            ):
                 if self.show_not_learning:
                     if self.use_subtypes:
                         data["00010,00001"] += 1
@@ -90,16 +96,14 @@ class LearningVisualizer:
                         data[10] += 1
                 continue
             else:
-                last_answers = (
-                    knowledges[record.question_id].responses
+                last_answers: list[LearningRecord] = (
+                    knowledges[record.question_id].records
                     if record.question_id in knowledges
                     else []
                 )
                 knowledges[record.question_id] = Knowledge(
                     record.question_id,
-                    last_answers + [record.response],
-                    record.time,
-                    record.interval,
+                    last_answers + [record],
                 )
                 data[compute_data_id()] += (
                     1 / (2 ** knowledges[record.question_id].get_depth())
@@ -125,14 +129,17 @@ class LearningVisualizer:
         # Plot data.
 
         for id_ in sorted(data.keys()):
-            if self.use_subtypes:
-                depth, returns = parse_data_id()
-            else:
-                depth, returns = id_, 1
-            max_depth = max(max_depth, depth)
+            if not self.by_language:
+                if self.use_subtypes:
+                    depth, returns = parse_data_id()
+                else:
+                    depth, returns = id_, 1
+                max_depth = max(max_depth, depth)
 
             color: str
-            if self.color_mode == "return_colors":
+            if self.by_language:
+                color = "#" + str(hex(abs(hash(id_))))[2:8]
+            elif self.color_mode == "return_colors":
                 number: int = max(0, 255 - returns * 0 - depth * 10)
                 color = (
                     f"#{hex(number)[2:]:>02}"
@@ -142,9 +149,16 @@ class LearningVisualizer:
             else:  # Depth colors.
                 color = DEPTH_COLORS[depth + 1]
 
+            label = None
+            if self.by_language:
+                label = id_
+
             if self.graph_mode == "fill_between":
                 plt.fill_between(
-                    x, [0] * (len(x) - len(y[id_])) + y[id_], color=color
+                    x,
+                    [0] * (len(x) - len(y[id_])) + y[id_],
+                    color=color,
+                    label=label,
                 )
             else:  # lines
                 plt.plot(
@@ -152,11 +166,14 @@ class LearningVisualizer:
                     [0] * (len(x) - len(y[id_])) + y[id_],
                     color=color,
                     linewidth=0.5,
+                    label=label,
                 )
 
         plt.title("Question depth")
         plt.xlabel("Time" if self.is_time else "Actions")
         plt.ylabel("Questions")
+        if self.by_language:
+            plt.legend(loc="upper left")
 
         if not self.is_time:
             plt.xlim(xmin=0)
