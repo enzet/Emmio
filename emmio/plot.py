48cc10031690efb2e1ac7da50a861247dfd36853
Sergey Vartanov <me@enzet.ru>
2024-01-03 23:35:56 +0400

    Rewrite custom graph drawing

--- emmio/plot.py
+++ emmio/plot.py
@@ -67,15 +67,13 @@ class Graph:
 
     def __init__(
         self,
-        data,
         min_x,
         max_x,
         canvas: Canvas = Canvas(),
         color: Color | list[Color] | str = Color("black"),
         background_color: Color = Color("white"),
-        grid_color: Color = Color("#DDDDDD"),
+        grid_color: Color = Color("#AAAAAA"),
     ):
-        self.data = data
         self.min_x = min_x
         self.max_x = max_x
         self.canvas: Canvas = canvas
@@ -85,6 +83,8 @@ class Graph:
         self.color: Color | list[Color] | str = color
         self.background_color: Color = background_color
         self.grid_color: Color = grid_color
+
+        self.last_text_y = 0
 
     def map_(self, point):
         """Map point into the canvas."""
@@ -96,7 +96,19 @@ class Graph:
             self.canvas.workspace[1],
         )
 
-    def plot(self, svg: Drawing) -> None:
+    def grid(self, svg):
+        svg.add(
+            svg.rect(
+                insert=(0, 0),
+                size=("100%", "100%"),
+                rx=None,
+                ry=None,
+                fill=self.background_color.hex,
+            )
+        )
+        self.draw_grid(svg)
+
+    def plot(self, svg: Drawing, data) -> None:
         recolor: str | None = None
 
         if isinstance(self.color, list):
@@ -113,55 +125,17 @@ class Graph:
             gradient: LinearGradient = svg.defs.add(linear_gradient)
             recolor = gradient.get_funciri()
 
-        if isinstance(self.color, Color):
-            recolor = self.color.hex
-
-        svg.add(
-            svg.rect(
-                insert=(0, 0),
-                size=("100%", "100%"),
-                rx=None,
-                ry=None,
-                fill=self.background_color.hex,
-            )
-        )
-        self.draw_grid(svg)
         last_text_y = 0
 
-        for xs, ys, color, title in self.data:
+        for xs, ys, color, title in data:
             if recolor:
                 color = recolor
+            if not color:
+                color = self.color
 
             assert len(xs) == len(ys)
 
-            xs_second: list[float] = [
-                (x - self.min_x).total_seconds() for x in xs
-            ]
-            points = []
-
-            for index, x in enumerate(xs_second):
-                y = ys[index]
-                mapped: np.ndarray = map_array(
-                    np.array((x, y)),
-                    np.array((self.min_x_second, self.max_y)),
-                    np.array((self.max_x_second, self.min_y)),
-                    self.canvas.workspace[0],
-                    self.canvas.workspace[1],
-                )
-                points.append(mapped)
-
-            previous_point: np.ndarray | None = None
-            for point in points:
-                if previous_point is not None:
-                    line: Line = svg.line(
-                        (previous_point[0], previous_point[1]),
-                        (point[0], point[1]),
-                        stroke=self.background_color.hex,
-                        stroke_width=6,
-                        stroke_linecap="round",
-                    )
-                    svg.add(line)
-                previous_point = point
+            points = self.to_points(xs, ys)
 
             previous_point: np.ndarray | None = None
             for point in points:
@@ -170,27 +144,82 @@ class Graph:
                         (previous_point[0], previous_point[1]),
                         (point[0], point[1]),
                         stroke=color,
-                        stroke_width=2,
+                        stroke_width=1,
                         stroke_linecap="round",
                     )
                     svg.add(line)
                 previous_point = point
 
-            # for point in points:
-            #     svg.add(
-            #         svg.circle(
-            #             (point[0], point[1]),
-            #             5.5,
-            #             fill=self.background_color.hex,
-            #         )
-            #     )
-            #     svg.add(svg.circle((point[0], point[1]), 3.5, fill=color))
+            title: str | None
+            if title:
+                text_y = max(last_text_y + 15, point[1] + 4)
+                self.text(svg, (point[0] + 10, text_y), title, color)
+                last_text_y = text_y
 
-            title: str
+    def to_points(self, xs, ys):
+        xs_second: list[float] = [(x - self.min_x).total_seconds() for x in xs]
+        points = []
+
+        for index, x in enumerate(xs_second):
+            y = ys[index]
+            mapped: np.ndarray = map_array(
+                np.array((x, y)),
+                np.array((self.min_x_second, self.max_y)),
+                np.array((self.max_x_second, self.min_y)),
+                self.canvas.workspace[0],
+                self.canvas.workspace[1],
+            )
+            points.append(mapped)
+        return points
+
+    def fill_between(
+        self, svg: Drawing, xs, ys_1, ys_2, color=None, label=None, opacity=None
+    ) -> None:
+        recolor: str | None = None
+
+        if isinstance(self.color, list):
+            linear_gradient: LinearGradient = svg.linearGradient(
+                self.map_((0, self.max_y)),
+                self.map_((0, 1)),
+                gradientUnits="userSpaceOnUse",
+            )
+            for index, color in enumerate(self.color):
+                linear_gradient.add_stop_color(
+                    index / (len(self.color) - 1), color.hex
+                )
+
+            gradient: LinearGradient = svg.defs.add(linear_gradient)
+            recolor = gradient.get_funciri()
+
+        last_text_y = 0
+
+        if recolor:
+            color = recolor
+        if not color:
+            color = self.color
+
+        assert len(xs) == len(ys_1)
+        assert len(xs) == len(ys_2)
+
+        points_1 = self.to_points(xs, ys_1)
+        points_2 = self.to_points(xs, ys_2)
+
+        points = points_1 + list(reversed(points_2))
+
+        d = f"M {points[0][0]},{points[0][1]}"
+        for point in points[1:]:
+            d += f" L {point[0]},{point[1]}"
+        d += " Z"
+
+        line: Line = svg.path(d=d, fill=color, opacity=opacity)
+        svg.add(line)
+        title: str | None
+        if label:
             text_y = max(last_text_y + 15, point[1] + 5)
-            self.text(svg, (point[0] + 15, text_y), title.upper(), color)
-            last_text_y = text_y
+            self.text(svg, (point[0] + 15, text_y), label, color)
+            self.last_text_y = text_y
 
+    def write(self, svg):
         with Path(svg.filename).open("w+") as output_file:
             svg.write(output_file)
 
@@ -198,11 +227,12 @@ class Graph:
         for index in range(8):
             mapped_1: np.ndarray = self.map_((0, index))
             mapped_2: np.ndarray = self.map_((self.max_x_second, index))
+            left = 85 if index in (0, 7) else 0
             line: Line = svg.line(
                 (mapped_1[0] - 30, mapped_1[1]),
-                (mapped_2[0], mapped_2[1]),
+                (mapped_2[0] + left, mapped_2[1]),
                 stroke=self.grid_color.hex,
-                stroke_width=2,
+                stroke_width=1,
             )
             if index != 0:
                 self.text(
@@ -237,9 +267,7 @@ class Graph:
             text,
             point,
             font_size=12,
-            font_family="Montserrat",
-            font_weight=600,
-            letter_spacing=1,
+            font_family="SF Pro",
             fill=color,
             text_anchor=anchor,
         )
