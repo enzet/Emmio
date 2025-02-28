d082573c5130e0923d3c43c842f9f74650c72d9a
Sergey Vartanov <me@enzet.ru>
2025-02-28 11:01:41 +0400

    Enable simple lexicon test

--- tests/test_lexicon.py
+++ tests/test_lexicon.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pytest
 
-from emmio.lexicon.config import LexiconConfig
+from emmio.lexicon.config import LexiconConfig, LexiconSelection
 from emmio.lexicon.core import (
     Lexicon,
     LexiconLog,
@@ -12,8 +12,8 @@ from emmio.lexicon.core import (
 )
 
 
-@pytest.mark.skip(reason="Fix lexicon creation.")
-def test_lexicon() -> None:
+@pytest.fixture
+def lexicon() -> Lexicon:
     """Test lexicon checking process."""
     temp_directory: Path = Path("/tmp") / "emmio_test_temp_directory"
     temp_directory.mkdir(exist_ok=True)
@@ -22,11 +22,16 @@ def test_lexicon() -> None:
         LexiconConfig(
             file_name="en.json",
             language="en",
-            frequency_list="en",
-            selection="arbitrary",
+            frequency_list={"id": "en"},
+            selection=LexiconSelection.ARBITRARY,
         ),
     )
-    lexicon.add_log(LexiconLog("log", WordSelection.ARBITRARY))
+    lexicon.log = LexiconLog()
+    return lexicon
+
+
+def test_register(lexicon: Lexicon) -> None:
+    """Test register."""
     lexicon.register("apple", LexiconResponse.KNOW, False, datetime(2000, 1, 1))
     assert len(lexicon.responses) == 1
     assert lexicon.has("apple")
