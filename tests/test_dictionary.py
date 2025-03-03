e267d095bb60568eeafbd413945b6d62db0ee73f
Sergey Vartanov <me@enzet.ru>
2025-03-04 00:35:22 +0400

    Add tests for dictionaries

---
+++ tests/test_dictionary.py
@@ -0,0 +1,18 @@
+from emmio.dictionary.core import Dictionary, SimpleDictionary
+from emmio.language import Language
+from pathlib import Path
+
+
+def test_dictionary() -> None:
+
+    dictionary: Dictionary = SimpleDictionary(
+        "test_dictionary",
+        Path("test/dictionary/test_dictionary.txt"),
+        "Test Dictionary",
+        {
+            "livre": "book",
+            "chat": "cat",
+        },
+        Language.from_code("fr"),
+        Language.from_code("en"),
+    )
